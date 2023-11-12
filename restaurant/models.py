6a22b64411fcaa0eb46fb0a2f46d04b91a0e5aa9
lamma-maihadisi <lammaedtech@gmail.com>
2023-11-12 17:52:48 +0100

    setup for the models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MenuTable(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2,max_digits=10)
+    inventory = models.IntegerField()
+
+class BookingTable(models.Model):
+    name = models.CharField(max_length=11)
+    no_of_guest = models.IntegerField()
+    booking_date = models.DateTimeField
+
