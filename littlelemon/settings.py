e9e3811ba7b3df8417555f54b1a4424a3b9198aa
lamma-maihadisi <lammaedtech@gmail.com>
2025-10-21 08:54:36 +0100

    settings.py debugged

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -81,15 +81,8 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'littleLemon',
-        'USER': 'root',
-        'PASSWORD': 'M33174798m,',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS':{
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES' "
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 database_url = os.environ.get('DATABASE_URL')
